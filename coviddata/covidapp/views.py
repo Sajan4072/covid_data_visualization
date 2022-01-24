746060c33f7fd5874d90cd2b2a954028eb17b212
Sajan4072 <rai4072@gmail.com>
2022-01-24 21:19:09 +0545

    1st step done

--- coviddata/covidapp/views.py
+++ coviddata/covidapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import urllib,json
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # Create your views here.
+
+def home(request):
+    url="https://api.covid19india.org/data.json"
+    res=urllib.request.urlopen(url)
+    data=json.loads(res.read())
+    return render(request,'home.html',{'data':data})
